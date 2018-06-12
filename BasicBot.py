589475199a6ea1bf6df0bfb1ebb9c39a1c3e7288
Kami <theonekamisama@gmail.com>
2018-06-12 18:44:38 +0530

    f

--- BasicBot.py
+++ BasicBot.py
@@ -51,7 +51,7 @@ async def hi(ctx):
 @client.command()
 async def age(ctx):
     x= ctx.guild.created_at()
-    await ctx.send(x)
+    await ctx.send(x.strftime(("%D")))
 
 @client.command()
 async def pick(ctx, *, args):
