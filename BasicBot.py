ba5bdc7c3decdac172c35721b59757bb9692ba4c
Kami <theonekamisama@gmail.com>
2018-06-22 14:13:43 +0530

    go

--- BasicBot.py
+++ BasicBot.py
@@ -72,15 +72,15 @@ async def randrace(ctx,args=1):
     'A pick device. Uses a list so i think any number of arguments can work'
     x=""
     for i in range(0,args):
-        x += random.choice(races)
+        x += random.choice(races)+ " "
     await ctx.send('Umm..I Picked: ' + x)
 
 @client.command()
-async def randrace(ctx,args=1):
+async def randpatron(ctx,args=1):
     'A pick device. Uses a list so i think any number of arguments can work'
     x=""
     for i in range(0,args):
-        x += random.choice(patron)
+        x += random.choice(patron)+" "
     await ctx.send('Umm..I Picked: ' + x)
 
     
