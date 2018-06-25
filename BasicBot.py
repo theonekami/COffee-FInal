565439390511312d796a94ea691c748427f8caea
Kami <theonekamisama@gmail.com>
2018-06-25 10:21:41 +0530

    go

--- BasicBot.py
+++ BasicBot.py
@@ -255,13 +255,13 @@ async def ships(ctx):
         """
         await ctx.send(x)
 
-##@client.command()
-##@commands.check(basic_check)
-##async def mute(ctx,args):
-##    for i in ctx.guild.members:
-##        if(i.mentioned_in(ctx.message)):
-##            await i.add_roles()
-##    await ctx.send("Muted Get REKT")
+@client.command()
+@commands.check(basic_check)
+async def mute(ctx,args):
+    for i in ctx.guild.members:
+        if(i.mentioned_in(ctx.message)):
+            await i.add_roles("Locked")
+    await ctx.send("Muted Get REKT")
 
 @client.command()
 @commands.check(basic_check)
