f41754e1ee4ecfcce66ceabbca008d23b4e21b16
Kami <theonekamisama@gmail.com>
2018-09-18 16:28:35 +0530

    go

--- Cool_Point.py
+++ Cool_Point.py
@@ -25,7 +25,7 @@ class Cool_Point:
 
     @cp.command(name="add")
     async def cp_add(self, ctx):
-        await self.Cp.insert(str(ctx.message.mentions[0].id) = [ctx.message.mentions[0].name,1])
+        await self.Cp.insert(ctx.message.mentions[0].id = [ctx.message.mentions[0].name,1])
         await ctx.send("Addded to"+ ctx.message.mentions[0].name)
 
     @cp.command(name="top")
