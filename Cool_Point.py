9904c6b00a21ba59f753327869ddfd59c72ccc18
Kami <theonekamisama@gmail.com>
2018-09-18 15:57:57 +0530

    go

--- Cool_Point.py
+++ Cool_Point.py
@@ -25,8 +25,8 @@ class Cool_Point:
 
     @cp.command(name="add")
     async def cp_add(self, ctx):
-        self.Cp.insert(kwargs={str(ctx.message.mentions[0]):1})
-        await ctx.send("Addded to"+ ctx.message.mentions[0])
+        self.Cp.insert(kwargs={str(ctx.message.mentions[0].id):1})
+        await ctx.send("Addded to"+ ctx.message.mentions[0].id)
 
 def setup(bot):
     bot.add_cog(Cool_Point(bot))
