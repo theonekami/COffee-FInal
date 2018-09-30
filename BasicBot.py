e83a551923caebf256668c728c9edcfa00a2927c
Kami <theonekamisama@gmail.com>
2018-09-30 19:46:51 +0530

    go

--- BasicBot.py
+++ BasicBot.py
@@ -307,7 +307,7 @@ async def ban(ctx):
 @commands.check(basic_check)
 async def unban(ctx):
     x= "BeGONe THOT!!!!"
-    await ctx.send(reverse(x))
+    await ctx.send("!!!!TOHT eNOGeB")
 
 ##@client.command()
 ##async def ask(ctx,*,args):
