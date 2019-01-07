485db651c0ca43d4cbb5194eb71c23b317fa397f
Kami <theonekamisama@gmail.com>
2019-01-07 16:17:28 +0530

    fo

--- doku.py
+++ doku.py
@@ -13,14 +13,7 @@ class Sudoku:
     async def doku(self,ctx):
         pass
 
-    @doku.command(name="start")
-    async def doku_start(self,ctx):
-        if(not (self.running)):
-            self.running=True
-        else:
-            await ctx.send("Sorry, a game is already running")
-            return
-        self.boardlist=sudotest.set_board()
+    async def write(self):
         y="```"
         u=0
         v=0
@@ -45,30 +38,27 @@ class Sudoku:
                 y+="________________________\n\n"
                 r=r+3              
         y+="```"
-        self.board_img=self.boardlist[0]
-        
-##        self.board_img="""```
-## ____________________
-##   A       B      C
-## ____________________
-## 1 1 1 | 2 2 2 | 3 3 3
-## 1 1 1 | 2 2 2 | 3 3 3
-## 1 1 1 | 2 2 2 | 3 3 3
-## ____________________
-##   D      E       F
-## ____________________
-## 1 1 1 | 2 2 2 | 3 3 3
-## 1 1 1 | 2 2 2 | 3 3 3
-## 1 1 1 | 2 2 2 | 3 3 3
-## ____________________
-##   G      H       I
-## ____________________
-## 1 1 1 | 2 2 2 | 3 3 3
-## 1 1 1 | 2 2 2 | 3 3 3
-## 1 1 1 | 2 2 2 | 3 3 3
-##```"""
-        await ctx.send(y)
 
+    return y
+
+    
+    @doku.command(name="start")
+    async def doku_start(self,ctx):
+        if(not (self.running)):
+            self.running=True
+        else:
+            await ctx.send("Sorry, a game is already running")
+            return
+        self.boardlist=sudotest.set_board()
+        self.board_img=self.boardlist[0]
+        await ctx.send(write())
+
+    @doku.command(name="show")
+    async def doku_show(self,ctx):
+        if(self.running):
+            await ctx.send(write())
+        else:
+            await ctx.send("Sorry, no game is running.")
         
 #[":arrow_upper_left::one::two::three:\n"],
 #[":one:",self.blank,self.blank,self.blank,"\n"],
