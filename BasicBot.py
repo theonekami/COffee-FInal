274acdb92f240e7a588f7af6dbc259e2958a86cb
theonekami <theonekamisama@gmail.com>
2019-03-10 16:15:08 +0530

    Update BasicBot.py

--- BasicBot.py
+++ BasicBot.py
@@ -330,7 +330,7 @@ async def git(ctx):
 
 @client.command()
 async def rickroll(ctx):
-    await ctx.message.author.send("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    await ctx.message.mentions[0].send("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
     await asyncio.sleep(5*60)
     await ctx.message.delete()
 
