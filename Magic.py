350abc299fe11a47379c54f1aeafd908c23f2fe5
theonekami <theonekamisama@gmail.com>
2019-04-09 14:10:08 +0530

    Update Magic.py

--- Magic.py
+++ Magic.py
@@ -46,10 +46,10 @@ class Magic(commands.Cog):
             em = discord.Embed(title=y['name'])
             if "card_faces" in y.keys():
                 for i in y["card_faces"]:
-                    em.set_image(url=i["image_uris"]['art_corp'])
+                    em.set_image(url=i["image_uris"]['border_crop'])
                     await ctx.send(embed= em)
             else:
-                em.set_image(url=y["image_uris"]['art_corp'])
+                em.set_image(url=y["image_uris"]['border_crop'])
                 await ctx.send(embed= em)
         elif(y['object']=="error"):
             await ctx.send(y["details"])
@@ -69,10 +69,10 @@ class Magic(commands.Cog):
             em = discord.Embed(title=y['name'])
             if "card_faces" in y.keys():
                 for i in y["card_faces"]:
-                    em.set_image(url=i["image_uris"]['border_crop'])
+                    em.set_image(url=i["image_uris"]['art_crop'])
                     await ctx.send(embed= em)
             else:
-                em.set_image(url=y["image_uris"]['border_crop'])
+                em.set_image(url=y["image_uris"]['art_crop'])
                 await ctx.send(embed= em)
         elif(y['object']=="error"):
             await ctx.send(y["details"])
