49327e4e0ab26100d6280d1a56f789eb75c784cb
theonekami <theonekamisama@gmail.com>
2019-11-12 15:42:48 +0530

    Update BasicBot.py

--- BasicBot.py
+++ BasicBot.py
@@ -92,9 +92,9 @@ async def pick(ctx, *, args):
 
 @client.command()
 async def roles(ctx, *, args):
-    for i in ctx.bot.get_cog("Role_command"):
-        for x in i.get_commands():
-            await ctx.send(x)
+    i= ctx.bot.get_cog("Role_command")
+    for x in i.get_commands():
+        await ctx.send(x)
 
     
 @client.command()
