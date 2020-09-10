1bf8739faa8c538582ed44e59828ec5e1bd8ed48
theonekami <theonekamisama@gmail.com>
2020-09-11 05:05:00 +0530

    Update BasicBot.py

--- BasicBot.py
+++ BasicBot.py
@@ -266,7 +266,7 @@ async def in_guilds(ctx):
 @commands.check(Kami_check)
 async def test(ctx):
     z=client.get_channel(id=753742549574746113)
-    z.send("For getting to magic react with <:mtg:753755564017975307>")
+    y=await z.send("For getting to magic react with <:mtg:753755564017975307>")
 ##    y= await z.fetch_message_fast(753746682574798948)
     await y.add_reaction("<:mtg:753755564017975307>")    
     
