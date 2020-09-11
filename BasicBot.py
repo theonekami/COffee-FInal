596f686c5aa22fbffa5b7d634269973e98a626d4
theonekami <theonekamisama@gmail.com>
2020-09-11 16:08:15 +0530

    Update BasicBot.py

--- BasicBot.py
+++ BasicBot.py
@@ -83,9 +83,11 @@ async def on_member_join(member):
 
 @client.event
 async def on_raw_reaction_add(payload):
+    if not(payload.message_id==753764595058999408):
+        return
     t=client.get_guild(ch_id)
     e=t.get_member(payload.user_id)
-    if payload.message_id==753764595058999408: #mtg
+    if payload.emoji.id==753755414310420489: #mtg
         await e.add_roles(t.get_role(643733493968535552))
         await e.send("Given the mtg role")
         #bad idea
