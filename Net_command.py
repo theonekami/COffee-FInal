e9e4f5669d1dceb6233705801c9258ab23c234e8
theonekami <theonekamisama@gmail.com>
2020-10-28 11:48:07 +0530

    Update Net_command.py

--- Net_command.py
+++ Net_command.py
@@ -44,6 +44,70 @@ class Net_Commands(commands.Cog):
         em.set_image(url=x[0])
         await ctx.send(embed= em)
 
+    @commands.command()
+    async def hug(self,ctx):
+        em=em = discord.Embed(title="HUGGGSSSSS!!!!")
+        async with aiohttp.request("get","https://some-random-api.ml/animu/hug") as res:
+            x=json.loads(await res.text())
+        res.close()
+        em.set_image(url=x["link"])
+        await ctx.send(embed=em)
+
+    @commands.command()
+    async def pet(self,ctx):
+        em=em = discord.Embed(title="There There")
+        async with aiohttp.request("get","https://some-random-api.ml/animu/pat") as res:
+            x=json.loads(await res.text())
+        res.close()
+        em.set_image(url=x["link"])
+        await ctx.send(embed=em)
+
+    @commands.command()
+    async def panda(self,ctx):
+        em=em = discord.Embed(title="PANDAAA!!!!")
+        async with aiohttp.request("get","https://some-random-api.ml/img/panda") as res:
+            x=json.loads(await res.text())
+        res.close()
+        em.set_image(url=x["link"])
+        await ctx.send(embed=em)
+
+    @commands.command()
+    async def red_panda(self,ctx):
+        em=em = discord.Embed(title="Red panda!!!!")
+        async with aiohttp.request("get","https://some-random-api.ml/img/red_panda") as res:
+            x=json.loads(await res.text())
+        res.close()
+        em.set_image(url=x["link"])
+        await ctx.send(embed=em)
+
+    @commands.command()
+    async def koala(self,ctx):
+        em=em = discord.Embed(title="Koala!!!!")
+        async with aiohttp.request("get","https://some-random-api.ml/img/koala") as res:
+            x=json.loads(await res.text())
+        res.close()
+        em.set_image(url=x["link"])
+        await ctx.send(embed=em)
+
+    @commands.command()
+    async def wink(self,ctx):
+        em=em = discord.Embed(title=";)")
+        async with aiohttp.request("get","https://some-random-api.ml/animu/wink") as res:
+            x=json.loads(await res.text())
+        res.close()
+        em.set_image(url=x["link"])
+        await ctx.send(embed=em)
+
+    @commands.command()
+    async def pika(self,ctx):
+        em=em = discord.Embed(title="pika!")
+        async with aiohttp.request("get","https://some-random-api.ml/img/pikachu") as res:
+            x=json.loads(await res.text())
+        res.close()
+        em.set_image(url=x["link"])
+        await ctx.send(embed=em)
+
+
 ##    @commands,command()
 ##    async def search(self, ctx,args):
 ##        x="https://www.google.co.in/search?q="+str(args)+"&rlz=1C1CHBF_enIN799IN799&oq=meems&aqs=chrome..69i57j0l5.806j0j7&sourceid=chrome&ie=UTF-8"
